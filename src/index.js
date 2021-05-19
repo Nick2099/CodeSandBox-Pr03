4c1e2fb7fce2841a4d35193f5637fd11e946b369
Nikica Dadic <75340386+Nick2099@users.noreply.github.com>
2021-05-19 22:33:07 +0200

    task done

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import ReactDOM from "react-dom";
 const tmpTime = new Date();
 const tmpHour = tmpTime.getHours();
 var tmpDayTime = "";
+var tmpStyle = {
+  color: "purple"
+};
 
 if (tmpHour >= 12 && tmpHour < 18) {
   tmpDayTime = "Good afternoon";
+  tmpStyle.color = "green";
 } else if (tmpHour >= 18) {
   tmpDayTime = "Good evening";
+  tmpStyle.color = "blue";
 } else {
   tmpDayTime = "Good morning";
+  tmpStyle.color  = "red";
 };
 
-ReactDOM.render(<h1>{tmpDayTime}</h1>, document.getElementById("root"));
+ReactDOM.render(<h1 style={tmpStyle}>{tmpDayTime}</h1>, document.getElementById("root"));
