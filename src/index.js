f7aff557c28c2b2c0d4bffb4707cde169b62e03a
Nikica Dadic <75340386+Nick2099@users.noreply.github.com>
2021-05-19 22:24:58 +0200

    task done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+const tmpTime = new Date();
+const tmpHour = tmpTime.getHours();
+var tmpDayTime = "";
 
+if (tmpHour >= 12 && tmpHour < 18) {
+  tmpDayTime = "Good afternoon";
+} else if (tmpHour >= 18) {
+  tmpDayTime = "Good evening";
+} else {
+  tmpDayTime = "Good morning";
+};
+
+ReactDOM.render(<h1>{tmpDayTime}</h1>, document.getElementById("root"));
